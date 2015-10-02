777216104ff6e1cd33e95cb5fac49c791fec0903
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-10-02 21:34:38 +0300

    libkmstest: add FAIL_IF()

--- libkmstest/test.h
+++ libkmstest/test.h
@@ -4,6 +4,8 @@
 #include "conv.h"
 #include "testpat.h"
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
 static void ASSERT_FAIL(const char *cond, const char *file,
@@ -18,4 +20,14 @@ static void ASSERT_FAIL(const char *cond, const char *file,
 
 #define ASSERT(x) if (unlikely(!(x))) { ASSERT_FAIL( __STRING(x), __FILE__, __LINE__, __PRETTY_FUNCTION__); }
 
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+static void FAIL_IF_FAIL(const char *txt, const char *file,
+			   unsigned int line, const char *func) __attribute__ ((__noreturn__));
+
+static void FAIL_IF_FAIL(const char *txt, const char *file,
+			   unsigned int line, const char *func)
+{
+	fprintf(stderr, "%s:%d: %s: FAIL: %s\n", file, line, func, txt);
+	abort();
+}
+
+#define FAIL_IF(x, y) if (unlikely(x)) { FAIL_IF_FAIL(y, __FILE__, __LINE__, __PRETTY_FUNCTION__); }

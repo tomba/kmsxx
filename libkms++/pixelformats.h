8bcc94ddddf93646ed8e9988a5b25f30938b7e20
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-10-03 23:26:06 +0300

    add PixelFormat

---
+++ libkms++/pixelformats.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace kms
+{
+constexpr uint32_t MakeFourCC(const char *fourcc)
+{
+	return fourcc[0] | (fourcc[1] << 8) | (fourcc[2] << 16) | (fourcc[3] << 24);
+}
+
+enum class PixelFormat : uint32_t
+{
+	NV12 = MakeFourCC("NV12"),
+	NV21 = MakeFourCC("NV21"),
+	UYVY = MakeFourCC("UYVY"),
+	YUYV = MakeFourCC("YUYV"),
+	XRGB8888 = MakeFourCC("XR24"),
+	RGB565 = MakeFourCC("RG16"),
+};
+
+static inline PixelFormat FourCCToPixelFormat(const std::string& fourcc)
+{
+	return (PixelFormat)MakeFourCC(fourcc.c_str());
+}
+
+}

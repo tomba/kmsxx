01cb65ace799d7b787275f1ee4810a3c3e8123d1
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-10-06 20:57:07 +0300

    test: FAIL_IF & ASSERT cleanup

--- libkmstest/test.h
+++ libkmstest/test.h
@@ -7,26 +7,14 @@
 
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
-static void ASSERT_FAIL(const char *cond, const char *file,
-			   unsigned int line, const char *func) __attribute__ ((__noreturn__));
+#define ASSERT(x) \
+	if (unlikely(!(x))) { \
+		fprintf(stderr, "%s:%d: %s: ASSERT(%s) failed\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, __STRING(x)); \
+		abort(); \
+	}
 
-static void ASSERT_FAIL(const char *cond, const char *file,
-			   unsigned int line, const char *func)
-{
-	fprintf(stderr, "%s:%d: %s: ASSERT(%s) failed\n", file, line, func, cond);
-	abort();
-}
-
-#define ASSERT(x) if (unlikely(!(x))) { ASSERT_FAIL( __STRING(x), __FILE__, __LINE__, __PRETTY_FUNCTION__); }
-
-static void FAIL_IF_FAIL(const char *txt, const char *file,
-			   unsigned int line, const char *func) __attribute__ ((__noreturn__));
-
-static void FAIL_IF_FAIL(const char *txt, const char *file,
-			   unsigned int line, const char *func)
-{
-	fprintf(stderr, "%s:%d: %s: FAIL: %s\n", file, line, func, txt);
-	abort();
-}
-
-#define FAIL_IF(x, y) if (unlikely(x)) { FAIL_IF_FAIL(y, __FILE__, __LINE__, __PRETTY_FUNCTION__); }
+#define FAIL_IF(x, fmt, ...) \
+	if (unlikely(x)) { \
+		fprintf(stderr, "%s:%d: %s:\n" fmt "\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__); \
+		abort(); \
+	}

7e8505c003c49898e6c668f070c579c8c03080f0
Tomi Valkeinen <tomi.valkeinen@ti.com>
2015-11-18 22:47:36 +0200

    kmstest: remove unused color_convert

--- libkmstest/kmstest.h
+++ libkmstest/kmstest.h
@@ -4,7 +4,6 @@ namespace kms
 {
 class DumbFramebuffer;
 
-void color_convert(DumbFramebuffer& dst, const DumbFramebuffer &src);
 void draw_color_bar(kms::DumbFramebuffer& buf, int old_xpos, int xpos, int width);
 void draw_test_pattern(DumbFramebuffer& fb);
 }

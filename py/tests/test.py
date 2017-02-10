bddac89707f53eb4f15386819faa2b76d590da21
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-02-10 11:15:35 +0200

    test.py: add dmabuf & omap options

--- py/tests/test.py
+++ py/tests/test.py
@@ -2,14 +2,33 @@
 
 import pykms
 
-card = pykms.Card()
+# draw test pattern via dmabuf?
+dmabuf = False
+
+# Use omap?
+omap = False
+
+if omap:
+	card = pykms.OmapCard()
+else:
+	card = pykms.Card()
+
 res = pykms.ResourceManager(card)
 conn = res.reserve_connector()
 crtc = res.reserve_crtc(conn)
-
 mode = conn.get_default_mode()
 
-fb = pykms.DumbFramebuffer(card, mode.hdisplay, mode.vdisplay, "XR24");
+if omap:
+	origfb = pykms.OmapFramebuffer(card, mode.hdisplay, mode.vdisplay, "XR24");
+else:
+	origfb = pykms.DumbFramebuffer(card, mode.hdisplay, mode.vdisplay, "XR24");
+
+if dmabuf:
+	fb = pykms.ExtFramebuffer(card, origfb.width, origfb.height, origfb.format,
+		[origfb.fd(0)], [origfb.stride(0)], [origfb.offset(0)])
+else:
+	fb = origfb
+
 pykms.draw_test_pattern(fb);
 
 crtc.set_mode(conn, fb, mode)

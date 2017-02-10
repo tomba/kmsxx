16ed78a1725d4c69dd5ff090f050673fb910a4d8
Tomi Valkeinen <tomi.valkeinen@ti.com>
2017-02-10 07:08:08 +0200

    ExtFb: remove legacy constructor

--- kms++/inc/kms++/extframebuffer.h
+++ kms++/inc/kms++/extframebuffer.h
@@ -9,7 +9,6 @@ namespace kms
 class ExtFramebuffer : public Framebuffer
 {
 public:
-	ExtFramebuffer(Card& card, uint32_t width, uint32_t height, uint8_t depth, uint8_t bpp, uint32_t stride, uint32_t handle);
 	ExtFramebuffer(Card& card, uint32_t width, uint32_t height, PixelFormat format,
 		       uint32_t handles[4], uint32_t pitches[4], uint32_t offsets[4]);
 	ExtFramebuffer(Card& card, uint32_t width, uint32_t height, PixelFormat format,
--- kms++/src/extframebuffer.cpp
+++ kms++/src/extframebuffer.cpp
@@ -12,17 +12,6 @@ using namespace std;
 namespace kms
 {
 
-ExtFramebuffer::ExtFramebuffer(Card& card, uint32_t width, uint32_t height, uint8_t depth, uint8_t bpp, uint32_t stride, uint32_t handle)
-	:Framebuffer(card, width, height)
-{
-	uint32_t id;
-	int r = drmModeAddFB(card.fd(), width, height, depth, bpp, stride, handle, &id);
-	if (r)
-		throw invalid_argument("fob");
-
-	set_id(id);
-}
-
 ExtFramebuffer::ExtFramebuffer(Card& card, uint32_t width, uint32_t height, PixelFormat format,
 			       uint32_t handles[], uint32_t pitches[], uint32_t offsets[])
 	: Framebuffer(card, width, height)

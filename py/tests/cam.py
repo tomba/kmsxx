a314a3a821db325f7f03161e8a656ae0ddda1ef3
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-02-09 14:37:29 +0200

    cam.py work

--- py/tests/cam.py
+++ py/tests/cam.py
@@ -5,6 +5,7 @@ import selectors
 import pykms
 import argparse
 import time
+from collections import deque
 
 #parser = argparse.ArgumentParser()
 #parser.add_argument("-d", "--device", type=str, default="/dev/video0")
@@ -55,11 +56,11 @@ res = pykms.ResourceManager(card)
 conn = res.reserve_connector()
 crtc = res.reserve_crtc(conn)
 plane1 = res.reserve_generic_plane(crtc, fmt)
-plane2 = res.reserve_generic_plane(crtc, pykms.PixelFormat.RGB565)
+plane2 = res.reserve_generic_plane(crtc, pykms.PixelFormat.YUYV)
 plane3 = res.reserve_generic_plane(crtc, fmt)
 plane4 = res.reserve_generic_plane(crtc, pykms.PixelFormat.RGB565)
 
-assert(plane4)
+assert(plane1 and plane2 and plane3 and plane4)
 
 mode = conn.get_default_mode()
 modeb = mode.to_blob(card)
@@ -74,33 +75,31 @@ NUM_BUFS = 5
 
 sensors = [
     { "idx": 0, "path": "/dev/video0", "plane": plane1, "x": 0, "y": 0, "hack": False },
-    { "idx": 1, "path": "/dev/video1", "plane": plane2, "x": mode.hdisplay - w, "y": 0, "hack": True },
-#    { "idx": 2, "path": "/dev/video2", "plane": plane3, "x": 0, "y": mode.vdisplay - h, "hack": False },
-#    { "idx": 3, "path": "/dev/video3", "plane": plane4, "x": mode.hdisplay - w, "y": mode.vdisplay - h, "hack": True },
+    { "idx": 1, "path": "/dev/video1", "plane": plane2, "x": mode.hdisplay - w, "y": 0, "hack": False },
+    { "idx": 2, "path": "/dev/video2", "plane": plane3, "x": 0, "y": mode.vdisplay - h, "hack": False },
+    { "idx": 3, "path": "/dev/video3", "plane": plane4, "x": mode.hdisplay - w, "y": mode.vdisplay - h, "hack": True },
 ]
 
 for data in sensors:
-    fbs = []
-    for i in range(NUM_BUFS):
-        fb = pykms.DumbFramebuffer(card, w, h, pykms.PixelFormat.RGB565 if data["hack"] else fmt)
-        fbs.append(fb)
-    data["fbs"] = fbs
-
     vd = pykms.VideoDevice(data["path"])
     cap = vd.capture_streamer
     cap.set_port(0)
     cap.set_format(fmt, w, h)
     cap.set_queue_size(NUM_BUFS)
 
-    for fb in fbs:
-        cap.queue(fb)
-
     data["vd"] = vd
     data["cap"] = cap
 
 for data in sensors:
-    fb = data["fbs"][0]
+    # Allocate FBs
+    fbs = []
+    for i in range(NUM_BUFS):
+        fb = pykms.DumbFramebuffer(card, w, h, pykms.PixelFormat.RGB565 if data["hack"] else fmt)
+        fbs.append(fb)
+    data["fbs"] = fbs
 
+    # Set fb0 to screen
+    fb = data["fbs"][0]
     plane = data["plane"]
 
     plane.set_props({
@@ -114,15 +113,27 @@ for data in sensors:
         "CRTC_H": fb.height,
     })
 
+    data["kms_old_fb"] = None
+    data["kms_fb"] = fb
+    data["kms_fb_queue"] = deque()
+
+    # Queue the rest to the camera
+    cap = data["cap"]
+    for i in range(1, NUM_BUFS):
+        cap.queue(fbs[i])
+
 for data in sensors:
     data["num_frames"] = 0
     data["time"] = time.perf_counter()
 
-    print("ENABLE STREAM")
+    #print("ENABLE STREAM")
+    print(f'{data["path"]}: stream on')
     data["cap"].stream_on()
     #time.sleep(0.5)
-    print("ENABLED STREAM")
+    #print("ENABLED STREAM")
     #time.sleep(1)
+
+kms_committed = False
 
 def readvid(data):
     data["num_frames"] += 1
@@ -130,7 +141,7 @@ def readvid(data):
     if data["num_frames"] == 100:
         diff = time.perf_counter() - data["time"]
 
-        print("{}: 100 frames in {}".format(data["idx"], diff))
+        print("{}: 100 frames in {:.2f} s, {:.2f} fps".format(data["path"], diff, 100 / diff))
 
         data["num_frames"] = 0
         data["time"] = time.perf_counter()
@@ -138,28 +149,75 @@ def readvid(data):
     cap = data["cap"]
     fb = cap.dequeue()
 
-    plane = data["plane"]
+    data["kms_fb_queue"].append(fb)
 
- #   req = pykms.AtomicReq(card)
- #   req.add(plane, "FB_ID", fb.id)
- #   req.commit(allow_modeset = False)
+    if len(data["kms_fb_queue"]) >= NUM_BUFS - 1:
+        print("WARNING fb_queue {}".format(len(data["kms_fb_queue"])))
 
-    cap.queue(fb)
+    #print(f'Buf from {data["path"]}: kms_fb_queue {len(data["kms_fb_queue"])}, commit ongoing {kms_committed}')
+
+    # XXX with a small delay we might get more planes to the commit
+    if kms_committed == False:
+        handle_pageflip()
 
 def readkey(conn, mask):
     for data in reversed(sensors):
-        print("DISABLE CAP")
+        print(f'{data["path"]}: stream off')
         data["cap"].stream_off()
         #time.sleep(0.5)
-        print("DISABLED CAP")
+        #print("DISABLED CAP")
         #time.sleep(1)
 
     print("Done")
     sys.stdin.readline()
     exit(0)
 
+def handle_pageflip():
+    global kms_committed
+
+    kms_committed = False
+
+    req = pykms.AtomicReq(card)
+
+    do_commit = False
+
+    for data in sensors:
+        #print(f'Page flip {data["path"]}: kms_fb_queue {len(data["kms_fb_queue"])}, new_fb {data["kms_fb"]}, old_fb {data["kms_old_fb"]}')
+
+        cap = data["cap"]
+
+        if data["kms_old_fb"]:
+            cap.queue(data["kms_old_fb"])
+            data["kms_old_fb"] = None
+
+        if len(data["kms_fb_queue"]) == 0:
+            continue
+
+        data["kms_old_fb"] = data["kms_fb"]
+
+        fb = data["kms_fb_queue"].popleft()
+        data["kms_fb"] = fb
+
+        plane = data["plane"]
+
+        req.add(plane, "FB_ID", fb.id)
+
+        do_commit = True
+
+    if do_commit:
+        req.commit(allow_modeset = False)
+        kms_committed = True
+
+
+def readdrm(fileobj, mask):
+    #print("EVENT");
+    for ev in card.read_events():
+        if ev.type == pykms.DrmEventType.FLIP_COMPLETE:
+            handle_pageflip()
+
 sel = selectors.DefaultSelector()
 sel.register(sys.stdin, selectors.EVENT_READ, readkey)
+sel.register(card.fd, selectors.EVENT_READ, readdrm)
 for data in sensors:
     sel.register(data["cap"].fd, selectors.EVENT_READ, lambda c,m,d=data: readvid(d))
 

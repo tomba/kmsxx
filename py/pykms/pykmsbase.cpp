c47a3b91eec9a5049a68d76004f7f8af29e2f077
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-04-26 16:15:13 +0300

    pykms: add media controller work

--- py/pykms/pykmsbase.cpp
+++ py/pykms/pykmsbase.cpp
@@ -222,7 +222,11 @@ void init_pykmsbase(py::module& m)
 		.value("ARGB2101010", PixelFormat::ARGB2101010)
 		.value("ABGR2101010", PixelFormat::ABGR2101010)
 		.value("RGBA1010102", PixelFormat::RGBA1010102)
-		.value("BGRA1010102", PixelFormat::BGRA1010102);
+		.value("BGRA1010102", PixelFormat::BGRA1010102)
+
+		.value("META_8", PixelFormat::META_8)
+		.value("META_16", PixelFormat::META_16);
+
 
 	m.def("fourcc_to_pixelformat", &FourCCToPixelFormat);
 

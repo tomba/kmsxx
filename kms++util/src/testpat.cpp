tomba/kmsxx

commit 40d96062a37ca924a66a926a234b35644928d4a7

Revert "testpat: remove threaded drawing"
This reverts commit 33246d9b5fb0347aabd62caac1da03440f9e1634.

Add threaded drawing back, but have it behind a CMAKE variable so that
it can easily be turned off.

Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

kms++util/src/testpat.cpp
@@ -4,6 +4,10 @@
 #include <cstring>
 #include <cassert>
 
+#ifdef HAS_PTHREAD
+#include <thread>
+#endif
+
 #include <kms++/kms++.h>
 #include <kms++util/kms++util.h>
 
@@ -152,7 +156,36 @@ static void draw_test_pattern_part(IFramebuffer& fb, unsigned start_y, unsigned 
 
 static void draw_test_pattern_impl(IFramebuffer& fb, YUVType yuvt)
 {
+#ifdef HAS_PTHREAD
+	if (fb.height() < 20) {
+		draw_test_pattern_part(fb, 0, fb.height(), yuvt);
+		return;
+	}
+
+	// Create the mmaps before starting the threads
+	for (unsigned i = 0; i < fb.num_planes(); ++i)
+		fb.map(0);
+
+	unsigned num_threads = thread::hardware_concurrency();
+	vector<thread> workers;
+
+	unsigned part = (fb.height() / num_threads) & ~1;
+
+	for (unsigned n = 0; n < num_threads; ++n) {
+		unsigned start = n * part;
+		unsigned end = start + part;
+
+		if (n == num_threads - 1)
+			end = fb.height();
+
+		workers.push_back(thread([&fb, start, end, yuvt]() { draw_test_pattern_part(fb, start, end, yuvt); }));
+	}
+
+	for (thread& t : workers)
+		t.join();
+#else
 	draw_test_pattern_part(fb, 0, fb.height(), yuvt);
+#endif
 }
 
 void draw_test_pattern(IFramebuffer &fb, YUVType yuvt)

CMakeLists.txt
@@ -25,6 +25,8 @@ set(KMSXX_PYTHON_VERSION "python3;python2" CACHE STRING "Python pkgconfig packag
 
 set(KMSXX_ENABLE_KMSCUBE OFF CACHE BOOL "Enable kmscube")
 
+set(KMSXX_ENABLE_THREADING ON CACHE BOOL "Enable threading for parallelized drawing")
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11 -Wall")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wextra -Wno-unused-parameter")
 

kms++util/CMakeLists.txt
@@ -9,6 +9,11 @@ target_include_directories(kms++util PUBLIC
 
 target_link_libraries(kms++util kms++)
 
+if (KMSXX_ENABLE_THREADING)
+    target_link_libraries(kms++util pthread)
+    add_definitions(-DHAS_PTHREAD)
+endif()
+
 set_target_properties(kms++util PROPERTIES
     PUBLIC_HEADER "${PUB_HDRS}")
 

commit 9ae90ce75478e49844cf984562db0dc1a074462f

testpat: Fix memory mapping in threaded drawing
The IFramebuffer::map() function is not thread-safe, which is why the
threaded implementation of draw_test_pattern_impl() maps all planes
before starting to draw. A typo slipped in the code, resulting in only
plane 0 being mapped. This didn't result in an immediate segfault, as
drawing operations in the worker threads map the remaining planes.
However, due to the implementation of DumbFramebuffer::map(), this can
result in the same plane being mapped multiple times, with only one of
the mapping recorded in the mapping cache. The other mappings are then
leaked, leading not only to extra memory consumption, but also to the
DRM device never being released even after the destruction of the Card
object.

Fix this.

Fixes: 40d96062a37c ("Revert "testpat: remove threaded drawing"")
Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

kms++util/src/testpat.cpp
@@ -173,7 +173,7 @@ static void draw_test_pattern_impl(IFramebuffer& fb, YUVType yuvt)
 
 	// Create the mmaps before starting the threads
 	for (unsigned i = 0; i < fb.num_planes(); ++i)
-		fb.map(0);
+		fb.map(i);
 
 	unsigned num_threads = thread::hardware_concurrency();
 	vector<thread> workers;